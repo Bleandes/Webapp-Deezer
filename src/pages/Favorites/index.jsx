58589b67009d84b74440ea4ba9809062bbec2f0f
Bleandes <Bleandes>
2021-10-08 04:12:29 -0300

    mock

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -140,6 +140,42 @@ const Favorites = () => {
           </svg>
         }
       />
+
+      {[1, 2, 3, 4, 5, 6, 7].map(() => (
+        <Item
+          title="Harder, Better, Faster, Stronger"
+          subtitle="Daft Punk"
+          thumbnail="https://api.deezer.com/album/302127/image"
+          iconfav={
+            <svg
+              width="14"
+              height="18"
+              viewBox="0 0 14 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 6V16H3V6H11ZM9.5 0H4.5L3.5 1H0V3H14V1H10.5L9.5 0ZM13 4H1V16C1 17.1 1.9 18 3 18H11C12.1 18 13 17.1 13 16V4Z"
+                fill="#8E8E8E"
+              />
+            </svg>
+          }
+          icondetail={
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 16H2V2H9V0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V9H16V16ZM11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"
+                fill="#A4A4A4"
+              />
+            </svg>
+          }
+        />
+      ))}
     </Page>
   );
 };
